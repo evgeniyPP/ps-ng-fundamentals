caad569491fba46daa8698317f8d8747d1d9ad42
evgeniyPP <aysanru@gmail.com>
2020-04-19 19:07:48 +0300

    Data & Toastr services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent,
+    EventDetailsComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
